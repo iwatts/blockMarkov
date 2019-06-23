cdf04f5790dcda5148920f3eedfb3626619014a6
Isaac <isaac.r.watts@gmail.com>
2019-06-23 14:37:25 -0600

    added result to end sentences

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
-function textToMarkov() {
+function markovMe() {
     const markovChain = {}
-    const textArr = ''
+    const textToMarkov = ''
 
-    for (let i = 0; i < textArr.length; i++) {
-      let word = textArr[i].toLowerCase().replace(/[\W_]/, "")
-      
-      if (!markovChain[word]) {
-        markovChain[word] = []
-        }
-      if (textArr[i + 1]) {
-        markovChain[word].push(textArr[i + 1].toLowerCase().replace(/[\W_]/, ""));
-  }
-  }
+    for (let i = 0; i < textToMarkov.length; i++) {
+		let word = textToMarkov[i].toLowerCase().replace(/[\W_]/, "")
+
+		if (!markovChain[word]) {
+			markovChain[word] = []
+		}
+		if (textToMarkov[i + 1]) {
+			markovChain[word].push(textToMarkov[i + 1].toLowerCase().replace(/[\W_]/, ""));
+  		}
+	}
+	  
     const words = Object.keys(markovChain)
     let word = words[Math.floor(Math.random() * words.length)]
     let result = ''
-    for (let i = 0; i < words.length; i++ ) {
-      result += word + ' ';
-      newWord =  markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
-      word = newWord;
-      if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
-    }
+	
+	for (let i = 0; i < words.length; i++ ) {
+		result += (word === '.') ? '. ': word + ' '
+		let newWord =  markovChain[word][Math.floor(Math.random() * markovChain[word].length)]
+		word = newWord;
+		if (!word || !markovChain.hasOwnProperty(word)) word = words[Math.floor(Math.random() * words.length)]
+	}
+	
     document.getElementById('markovResults').innerText = result;
-  }
+}
